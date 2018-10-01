2ae75ddcbd712260123add89226f4ee4be293a60
Suchit Jain <35426827+suchitj2702@users.noreply.github.com>
2018-10-01 23:52:44 +0530

    Add files via upload

---
+++ maskrcnninferrence.py
@@ -0,0 +1,155 @@
+# @Author: Suchit Jain
+# @Date:   2018-08-14T08:54:58+05:30
+# @Email:  suchit27022@gmail.com
+# @Last modified by:   Suchit Jain
+# @Last modified time: 2018-10-01T18:12:44+05:30
+# @License: Free
+
+""" Usage: Run from command line as such(recommended python 3.6.5):
+
+    # Using trained coco weights for inferrence
+    python maskrcnninferrence.py coco
+
+    # Using the last saved weights by maskrcnntrain for inferrence
+    python maskrcnninferrence.py last
+
+    # Using costum trained weights for inferrence
+    python maskrcnninferrence.py costum
+"""
+import cv2
+import numpy as np
+import maskrcnntrain
+from mrcnn import utils
+from mrcnn import model as modellib
+import os
+import json
+import pandas as pd
+import coco
+
+LOGS_FOLDER = "logs"
+PRETRAINED_COCO_WEIGHTS = "mask_rcnn_coco.h5"
+TRAINED_MASKRCNN_WEIGHTS = "trainedweights.h5"
+CLASSES = 'class1' # use a list of classes if multiple classes
+IMG_DIR = "TestImages/Images"
+INFERRED_DIR = "TestImages/InferredImages"
+
+class InferenceConfigtrained(maskrcnntrain.trainingconfig):
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    DETECTION_MIN_CONFIDENCE = 0.9 # Skip detections with < 90 % confidence
+
+class InferenceConfigcoco(coco.CocoConfig):
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+def load_weights():
+    global class_names, weights_path, model
+    if WEIGHTS_TO_USE == "coco":
+        config = InferenceConfigcoco()
+        config.display()
+        model = modellib.MaskRCNN(
+            mode = "inference", model_dir = LOGS_FOLDER, config = config
+        )
+        weights_path = PRETRAINED_COCO_WEIGHTS
+        model.load_weights(weights_path, by_name=True)
+        class_names = [
+            'BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+            'bus', 'train', 'truck', 'boat', 'traffic light',
+            'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+            'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+            'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+            'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+            'kite', 'baseball bat', 'baseball glove', 'skateboard',
+            'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+            'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+            'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+            'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+            'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+            'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+            'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+            'teddy bear', 'hair drier', 'toothbrush'
+        ]
+    elif WEIGHTS_TO_USE == "last":
+        config = InferenceConfigtrained()
+        config.display()
+        model = modellib.MaskRCNN(
+            mode = "inference", model_dir = LOGS_FOLDER, config = config
+        )
+        weights_path = model.find_last()[1]
+        model.load_weights(weights_path, by_name=True)
+        class_names = CLASSES
+
+    else:
+        config = InferenceConfigtrained()
+        config.display()
+        model = modellib.MaskRCNN(
+            mode = "inference", model_dir = LOGS_FOLDER, config = config
+        )
+        weights_path = TRAINED_MASKRCNN_WEIGHTS
+        model.load_weights(weights_path, by_name=True)
+        class_names = CLASSES
+
+def random_colors(N):
+    np.random.seed(1)
+    colors = [tuple(255 * np.random.rand(3)) for _ in range(N)]
+    return colors
+
+
+def apply_mask(image, mask, color, alpha = 0.5):
+    """apply mask to image"""
+    for n, c in enumerate(color):
+        image[:, :, n] = np.where(
+            mask == 1,
+            image[:, :, n] * (1 - alpha) + alpha *c,
+            image[:, :, n]
+        )
+    return image
+
+def display_instances(image, boxes, masks, ids, names, scores):
+    """
+        take the image and results and apply the mask, box, and label
+    """
+    n_instances = boxes.shape[0]
+
+    if not n_instances:
+        print('NO INSTANCES TO DISPLAY')
+    else:
+        assert boxes.shape[0] == masks.shape[-1] == ids.shape[0]
+
+    for i in range(n_instances):
+        if not np.any(boxes[i]):
+            continue
+        y1, x1, y2, x2 = boxes[i]
+        label = names[ids[i]]
+        color = class_dict[label]
+        score = scores[i] if scores is not None else None
+        caption = '{}{:.2f}'.format(label, score) if score else label
+        mask = masks[:, :, i]
+        image = apply_mask(image, mask, color)
+        image =cv2.rectangle(image, (x1, y1), (x2, y2), color, 2)
+        image = cv2.putText(image, caption, (x1, y1), cv2.FONT_HERSHEY_COMPLEX, 0.5, color, 2)
+
+    return image, n_instances
+
+def main():
+    for filename in os.listdir(IMG_DIR):
+        test_image = cv2.imread(os.path.join(IMG_DIR,filename))
+        results = model.detect([test_image], verbose = 0)
+        r = results[0]
+        inferred, number_of_instances = display_instances(
+            test_image, r['rois'], r['masks'], r['class_ids'], class_names, r['scores']
+        )
+        cv2.imwrite(os.path.join(INFERRED_DIR,filename), inferred)
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('weights_to_use',
+                        metavar = "<weights_to_use>",
+                        help = 'Can either be coco, last or costum')
+    args = parser.parse_args()
+    WEIGHTS_TO_USE = args.weights_to_use
+    load_weights()
+    colors = random_colors(len(class_names))
+    class_dict = {name: color for name, color in zip(class_names, colors)}
+    main()
--- maskrcnntrain.py
+++ maskrcnntrain.py
@@ -1,3 +1,18 @@
+# @Author: Suchit Jain
+# @Date:   2018-08-13T20:08:01+05:30
+# @Email:  suchit27022@gmail.com
+# @Last modified by:   Suchit Jain
+# @Last modified time: 2018-10-01T18:12:26+05:30
+# @License: Free
+
+""" Usage: Run from command line as such(recommended python 3.6.5):
+
+    # Train a new model starting from pre-trained COCO weights
+    python maskrcnntrain.py coco
+
+    # Continue training a model that you had trained earlier
+    python maskrcnntrain.py last
+"""
 import json
 import datetime
 import numpy as np
@@ -9,51 +24,54 @@ from mrcnn import model as modellib, utils
 PRETRAINED_COCO_WEIGHTS = "mask_rcnn_coco.h5"
 WEIGHTS_TO_USE = "coco" # Can be either coco or last(for continuing training)
 LOGS_FOLDER = "logs"
-DATASET_FOLDER = "datasetnew"
-CLASS = "building/plot"
+DATASET_FOLDER = "TrainingImages"
+REGION_JSON = "RegionJson.json"
+CLASSES = ['class1', 'class2', 'class3'] # list of classes
 
 class trainingconfig(Config):
     """
     Derives from the base Config class and overrides some values
     """
-    NAME = "object_detector"
+    NAME = "Class"
 
     IMAGES_PER_GPU = 2
 
-    NUM_CLASSES = 2 # For single class classification(including background)
+    NUM_CLASSES = len(CLASSES)
 
     STEPS_PER_EPOCH = 100
 
-    DETECTION_MIN_CONFIDENCE = 0.9 # Skip detections with < 90 % confidence
-
 class Dataset(utils.Dataset):
     def load_dataset(self, dataset_dir, subset):
-        self.add_class(CLASS, 1, CLASS)
+        for i in range(0,len(CLASSES)-1,1):
+            print(i)
+            self.add_class("Class", i + 1, CLASSES[i])
 
         assert subset in ["train", "val"]
         dataset_dir = dataset_dir + "/" + subset
-        annotations = json.load(open(dataset_dir + "/" + "datasetroad.json"))
+        annotations = json.load(open(dataset_dir + "/" +  REGION_JSON))
         annotations = list(annotations.values())
         annotations = [a for a in annotations if a['regions']]
 
         for a in annotations:
             polygons = [r['shape_attributes'] for r in a['regions'].values()]
+            objectclass =  [r['region_attributes'] for r in a['regions'].values()]
             image_path = dataset_dir + "/" + a['filename']
             image = skimage.io.imread(image_path)
             height, width = image.shape[:2]
 
             self.add_image(
-                CLASS,
+                "Class",
                 image_id = a['filename'],
                 path = image_path,
                 width = width,
                 height = height,
-                polygons = polygons)
+                polygons = polygons,
+                objectclass = objectclass)
 
     def load_mask(self, image_id):
         # If not an dataset image, delegate to parent class.
         image_info = self.image_info[image_id]
-        if image_info["source"] != CLASS:
+        if image_info["source"] != "Class":
             return super(self.__class__, self).load_mask(image_id)
         # Convert polygons to a bitmap mask of shape
         # [height, width, instanc_count]
@@ -61,20 +79,29 @@ class Dataset(utils.Dataset):
         mask = np.zeros([info["height"], info["width"], len(info["polygons"])]
                         ,dtype = np.uint8)
 
+        classmap = np.zeros(len(info["polygons"]))
+
+        for n, c in enumerate(info["objectclass"]):
+            classnum = 1
+            for i in CLASSES:
+                if c['Class'] == i:
+                    break
+                classnum = classnum + 1
+            classmap[n] = classnum
+
         for i, p in enumerate(info["polygons"]):
             # Get indexes of pixels inside the polygon and set them to 1
             rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
             mask[rr, cc, i] = 1
 
-        # Return mask, and array of class IDs of each instance. Since we have
-        # one class ID only, we return an array of 1s
-        return mask.astype(np.bool), np.ones([mask.shape[-1]], dtype = np.int32)
+        # Return mask, and array of class IDs of each instance.
+        return mask.astype(np.bool), classmap.astype(np.int32)
 
     def image_reference(self, image_id):
         """Return the path of the image"""
         info = self.image_info[image_id]
-        if info["source"] == CLASS:
-            return info["path"]
+        if info["source"] == "Class":
+            return info["Class"]
         else:
             super(self.__class__, self).image_reference(image_id)
 
@@ -99,11 +126,18 @@ def train(model):
 
 
 if __name__ == '__main__':
+    import asgparse
+    parser = argparse.ArgumentParser()
+
     # Configuration for the training
     config = trainingconfig()
     # Create Model
     model = modellib.MaskRCNN(mode = "training", config = config, model_dir = LOGS_FOLDER)
-
+    parser.add_argument('weights_to_use',
+                        metavar = "<weights_to_use>",
+                        help = 'Can either be coco or last')
+    args = parser.parse_args()
+    WEIGHTS_TO_USE = args.weights_to_use
     # Load Pretrained Weights
     if WEIGHTS_TO_USE == "coco":
         weights_path = PRETRAINED_COCO_WEIGHTS
